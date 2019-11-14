5993655c6b10d4d48dbff6a82f6044200d66b5e5
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-14 14:32:54 +0200

    Reducer updateLog - action / reducer / connect

--- projects/it-logger/src/actions/logActions.js
+++ projects/it-logger/src/actions/logActions.js
@@ -1,4 +1,12 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
+} from "./types";
 
 // Get logs from server
 export const getLogs = () => async dispatch => {
@@ -47,14 +55,13 @@ export const addLog = log => async dispatch => {
 };
 
 // Delete log from server
-export const deleteLog = (id) => async dispatch => {
+export const deleteLog = id => async dispatch => {
   try {
     setLoading();
 
     await fetch(`/logs/${id}`, {
-      method: 'DELETE'
+      method: "DELETE"
     });
-    
 
     dispatch({
       type: DELETE_LOG,
@@ -66,6 +73,47 @@ export const deleteLog = (id) => async dispatch => {
       payload: error.respons.data
     });
   }
+};
+
+// Update log "Edit"
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: "PUT",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.respons.data
+    });
+  }
+};
+
+// Set current log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+};
+
+// Clear current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
 };
 
 // Set loading to true
