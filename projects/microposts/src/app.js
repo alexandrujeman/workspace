f525b410309a2b47fd4604dc0b0f721fc9a6bab0
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-25 23:57:22 +0200

    Add micropost submit

--- projects/microposts/src/app.js
+++ projects/microposts/src/app.js
@@ -6,8 +6,31 @@ import { ui } from "./ui";
 // Het post on DOM load
 document.addEventListener('DOMContentLoaded', getPosts)
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', addPost)
+
+// Get posts
 function getPosts() {
   http.get("http://localhost:3000/posts")
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
+}
+
+// Submit post
+function addPost() {
+  const title = document.querySelector('#title').value
+  const body = document.querySelector('#body').value
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+  .then(data => {
+    ui.showAlert('Post added', 'alert alert-success');
+    ui.clearFields();
+    getPosts()
+  }).catch(err => console.log(err))
 }
