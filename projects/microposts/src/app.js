915fe19575e3f954b6064e17d08729d7ebe8474e
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-26 12:49:24 +0200

    Add microposts and show alert

--- projects/microposts/src/app.js
+++ projects/microposts/src/app.js
@@ -3,34 +3,37 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import { ui } from "./ui";
 
-// Het post on DOM load
-document.addEventListener('DOMContentLoaded', getPosts)
+// Get posts on DOM load
+document.addEventListener("DOMContentLoaded", getPosts);
 
 // Listen for add post
-document.querySelector('.post-submit').addEventListener('click', addPost)
+document.querySelector(".post-submit").addEventListener("click", submitPost);
 
 // Get posts
 function getPosts() {
-  http.get("http://localhost:3000/posts")
+  http
+    .get("http://localhost:3000/posts")
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
 }
 
-// Submit post
-function addPost() {
-  const title = document.querySelector('#title').value
-  const body = document.querySelector('#body').value
+// Submit Post
+function submitPost() {
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
 
   const data = {
     title,
     body
-  }
+  };
 
   // Create Post
-  http.post('http://localhost:3000/posts', data)
-  .then(data => {
-    ui.showAlert('Post added', 'alert alert-success');
-    ui.clearFields();
-    getPosts()
-  }).catch(err => console.log(err))
+  http
+    .post("http://localhost:3000/posts", data)
+    .then(data => {
+      ui.showAlert("Post added", "alert alert-success");
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
 }
